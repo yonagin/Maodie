f60b4c5323f53fd7890b258326714af8fe08ef3f
yonagin <3118391854@qq.com>
2025-10-07 22:01:47 +0800

    initial

--- taming/data/utils.py
+++ taming/data/utils.py
@@ -8,7 +8,8 @@ from pathlib import Path
 import numpy as np
 import torch
 from taming.data.helper_types import Annotation
-from torch._six import string_classes
+# 修复torch._six导入问题
+string_classes = (str, bytes)
 from torch.utils.data._utils.collate import np_str_obj_array_pattern, default_collate_err_msg_format
 from tqdm import tqdm
 
